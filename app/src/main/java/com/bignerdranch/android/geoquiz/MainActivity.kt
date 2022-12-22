8e2338bec1d3819cdd07e4ae26aaad848cc17c53
Michael Fernández <mfernandezm85@gmail.com>
2022-12-22 17:12:12 -0500

    Added more questions and ViewBinding

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -5,20 +5,34 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+// View Binding - setup
+import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    // View Binding - setup
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        // View Binding - setup
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton.setOnClickListener { view: View ->
+        // View Binding - use
+        binding.trueButton.setOnClickListener { view: View ->
             // Do something in response to the click here
             Toast.makeText(
                 this,
@@ -27,7 +41,8 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
 
-        falseButton.setOnClickListener { view: View ->
+        // View Binding - use
+        binding.falseButton.setOnClickListener { view: View ->
             // Do something in response to the click here
             Toast.makeText(
                 this,
