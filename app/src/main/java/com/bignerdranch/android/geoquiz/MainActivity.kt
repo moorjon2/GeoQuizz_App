c83dced554ba59c6cba902e14d3b94cff6795282
Michael Fernandez <mfernandezm85@gmail.com>
2023-01-06 23:16:02 -0500

    Added a cheat button and another Activity

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.bignerdranch.android.geoquiz
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 // View Binding - setup
 import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
@@ -20,6 +23,16 @@ class MainActivity : AppCompatActivity() {
     // Adding QuizViewModel
     // by keyword is a property delegate, lazy is also a commonly used property delegate
     private val quizViewModel: QuizViewModel by viewModels()
+
+    private val cheatLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        // Handle the result
+        if (result.resultCode == Activity.RESULT_OK) {
+            quizViewModel.isCheater =
+                result.data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,10 +63,17 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
-        binding.previousButton.setOnClickListener {
-            quizViewModel.moveToPrev()
-            updateQuestion()
+        binding.cheatButton.setOnClickListener {
+            // Start CheatActivity
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            cheatLauncher.launch(intent)
         }
+
+//        binding.previousButton.setOnClickListener {
+//            quizViewModel.moveToPrev()
+//            updateQuestion()
+//        }
 
         updateQuestion()
 
@@ -92,10 +112,10 @@ class MainActivity : AppCompatActivity() {
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = quizViewModel.currentQuestionAnswer
 
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
 
         Toast.makeText(
